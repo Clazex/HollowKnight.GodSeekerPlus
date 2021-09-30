d161f475731ebb9d04132eeef21c34e604880b0c
Frank Yang <clazex.y@gmail.com>
2021-09-30 22:45:40 +0800

    chore: add settings

---
+++ GodSeekerPlus/GlobalSettings.cs
@@ -0,0 +1,4 @@
+namespace GodSeekerPlus {
+	public class GlobalSettings {
+	}
+}
--- GodSeekerPlus/GodSeekerPlus.cs
+++ GodSeekerPlus/GodSeekerPlus.cs
@@ -2,9 +2,12 @@ using System.Reflection;
 using Modding;
 
 namespace GodSeekerPlus {
-	public class GodSeekerPlus : Mod {
+	public class GodSeekerPlus : Mod, IGlobalSettings<GlobalSettings>, ILocalSettings<LocalSettings> {
 		public static GodSeekerPlus LoadedInstance { get; private set; }
 		public override string GetVersion() => "0.1.0";
+
+		public GlobalSettings GlobalSettings { get; set; } = new GlobalSettings();
+		public LocalSettings LocalSettings { get; set; } = new LocalSettings();
 
 
 		public override void Initialize() {
@@ -27,5 +30,14 @@ namespace GodSeekerPlus {
 		private void Hook() {}
 
 		private void UnHook() {}
+
+
+		public void OnLoadGlobal(GlobalSettings s) => GlobalSettings = s;
+
+		public GlobalSettings OnSaveGlobal() => GlobalSettings;
+
+		public void OnLoadLocal(LocalSettings s) => LocalSettings = s;
+
+		public LocalSettings OnSaveLocal() => LocalSettings;
 	}
 }
