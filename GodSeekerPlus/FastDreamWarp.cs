0c4fe7f7d8e06e017cf666c32c4a91b6757a2c08
Frank Yang <clazex.y@gmail.com>
2021-10-02 14:06:55 +0800

    fix: unintendedly touched fsm

--- GodSeekerPlus/FastDreamWarp.cs
+++ GodSeekerPlus/FastDreamWarp.cs
@@ -9,9 +9,7 @@ namespace GodSeekerPlus {
 		public static void UnHook() => On.PlayMakerFSM.OnEnable -= ModifyFSM;
 
 		private static void ModifyFSM(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
-			orig(self);
-
-			if (self.gameObject.name == "Knight" || self.FsmName == "Dream Nail") {
+			if (self.gameObject.name == "Knight" && self.FsmName == "Dream Nail") {
 				FsmState stateWarpCharge = FsmUtil.GetState(self, "Warp Charge");
 				FsmUtil.InsertAction(stateWarpCharge, 0, new GGCheckIfBossScene {
 					// If in boss scene, fire CHARGED event immediately
@@ -20,6 +18,8 @@ namespace GodSeekerPlus {
 
 				GodSeekerPlus.Instance.Log("Dream Nail FSM modified");
 			}
+
+			orig(self);
 		}
 	}
 }
