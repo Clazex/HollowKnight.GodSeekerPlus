52d26e5ff8ad453df273c66b67f40c888d05b887
Frank Yang <clazex.y@gmail.com>
2021-10-16 22:48:20 +0800

    refactor: use local variable for frame rate limit time

--- GodSeekerPlus/Modules/FrameRateLimit.cs
+++ GodSeekerPlus/Modules/FrameRateLimit.cs
@@ -3,12 +3,17 @@ using Modding;
 
 namespace GodSeekerPlus.Modules {
 	internal sealed class FrameRateLimit : Module {
-		private protected override void Load() => ModHooks.HeroUpdateHook += ThreadSleep;
+		private static int time = 50;
+
+		private protected override void Load() {
+			time = 10 * GodSeekerPlus.Instance.GlobalSettings.frameRateLimitMultiplier;
+			ModHooks.HeroUpdateHook += ThreadSleep;
+		}
 
 		private protected override void Unload() => ModHooks.HeroUpdateHook -= ThreadSleep;
 
 		private protected override bool ShouldLoad() => GodSeekerPlus.Instance.GlobalSettings.frameRateLimit;
 
-		private static void ThreadSleep() => Thread.Sleep(10 * GodSeekerPlus.Instance.GlobalSettings.frameRateLimitMultiplier);
+		private static void ThreadSleep() => Thread.Sleep(time);
 	}
 }
