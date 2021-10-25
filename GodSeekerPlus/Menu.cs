e9536c2117c0d424ad1fe40042519dc5163fe3b2
Frank Yang <clazex.y@gmail.com>
2021-10-25 18:40:41 +0800

    feat: add localization interface

--- GodSeekerPlus/Menu.cs
+++ GodSeekerPlus/Menu.cs
@@ -3,27 +3,31 @@ using System.Linq;
 using GodSeekerPlus.Util;
 using Modding;
 using static Modding.IMenuMod;
+using Lang = Language.Language;
 
 namespace GodSeekerPlus {
 	public sealed partial class GodSeekerPlus : IMenuMod {
 		bool IMenuMod.ToggleButtonInsideMenu => false;
 
-		List<MenuEntry> IMenuMod.GetMenuData(MenuEntry? _) {
-			string[] states = {
-				Language.Language.Get("MOH_OFF", "MainMenu"),
-				Language.Language.Get("MOH_ON", "MainMenu")
-			};
+		private static string[] States { get; } = new string[] {
+			Lang.Get("MOH_OFF", "MainMenu"),
+			Lang.Get("MOH_ON", "MainMenu")
+		};
 
-			return ModuleHelper
-				.GetToggleableModuleNames()
-				.Map<string, MenuEntry>(name => new(
-					name,
-					states,
-					"",
-					(val) => moduleManager.Modules[name].Enabled = val != 0,
-					() => moduleManager.Modules[name].Enabled ? 1 : 0
-				))
-				.ToList();
+		private string GetLocalizedName(string name) {
+			string localizedName = Lang.Get(name, GetName());
+			return localizedName.Any() ? localizedName : name;
 		}
+
+		List<MenuEntry> IMenuMod.GetMenuData(MenuEntry? _) => ModuleHelper
+			.GetToggleableModuleNames()
+			.Map(name => new MenuEntry(
+				GetLocalizedName(name),
+				States,
+				"",
+				(val) => moduleManager.Modules[name].Enabled = val != 0,
+				() => moduleManager.Modules[name].Enabled ? 1 : 0
+			))
+			.ToList();
 	}
 }
