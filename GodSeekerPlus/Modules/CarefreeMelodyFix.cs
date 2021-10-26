86a0f872180098fb368d7fb209bf4c63b67f208a
Frank Yang <clazex.y@gmail.com>
2021-10-26 22:28:38 +0800

    refactor: access field directly

--- GodSeekerPlus/Modules/CarefreeMelodyFix.cs
+++ GodSeekerPlus/Modules/CarefreeMelodyFix.cs
@@ -4,15 +4,13 @@ using Logger = GodSeekerPlus.Util.Logger;
 namespace GodSeekerPlus.Modules {
 	[Module(toggleable = true, defaultEnabled = true)]
 	internal sealed class CarefreeMelodyFix : Module {
-		private const string targetEntry = "destroyedNightmareLantern";
-
 		private protected override void Load() => ModHooks.CharmUpdateHook += WatchAndFixCarefreeMelody;
 
 		private protected override void Unload() => ModHooks.CharmUpdateHook -= WatchAndFixCarefreeMelody;
 
 		private static void WatchAndFixCarefreeMelody(PlayerData data, HeroController controller) {
-			if (controller.carefreeShieldEquipped && !data.GetBool(targetEntry)) {
-				data.SetBool(targetEntry, true);
+			if (controller.carefreeShieldEquipped && !data.destroyedNightmareLantern) {
+				data.destroyedNightmareLantern = true;
 
 				Logger.LogDebug("Carefree Melody fixed");
 			}
