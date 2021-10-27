bbc7d8c45df44b3dac078badb837d624e56fa984
Frank Yang <clazex.y@gmail.com>
2021-10-27 23:06:37 +0800

    fix: remove mod name l11n

--- GodSeekerPlus/GodSeekerPlus.cs
+++ GodSeekerPlus/GodSeekerPlus.cs
@@ -6,9 +6,6 @@ namespace GodSeekerPlus {
 	public sealed partial class GodSeekerPlus : Mod {
 		public static GodSeekerPlus Instance { get; private set; }
 
-		public GodSeekerPlus() : base(L11nUtil.Localize("GodSeekerPlus")) { }
-
-
 		public override string GetVersion() => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
 
 		private readonly ModuleManager moduleManager = new();
