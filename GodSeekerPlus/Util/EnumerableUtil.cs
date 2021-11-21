bc2082943e6eeaeb819959ab581bdb99acb60f6a
Frank Yang <clazex.y@gmail.com>
2021-11-21 21:42:12 +0800

    perf: use standard lib funcs

--- GodSeekerPlus/Util/EnumerableUtil.cs
+++ GodSeekerPlus/Util/EnumerableUtil.cs
@@ -1,24 +1,17 @@
 using System;
 using System.Collections.Generic;
+using Enumerable = System.Linq.Enumerable;
 
 namespace GodSeekerPlus.Util {
 	internal static class IEnumerableUtil {
 		internal static bool Any<T>(this IEnumerable<T> self) =>
-			System.Linq.Enumerable.Any(self);
+			Enumerable.Any(self);
 
-		internal static IEnumerable<U> Map<T, U>(this IEnumerable<T> self, Func<T, U> f) {
-			foreach (T i in self) {
-				yield return f(i);
-			}
-		}
+		internal static IEnumerable<U> Map<T, U>(this IEnumerable<T> self, Func<T, U> f) =>
+			Enumerable.Select(self, f);
 
-		internal static IEnumerable<T> Filter<T>(this IEnumerable<T> self, Predicate<T> predicate) {
-			foreach (T i in self) {
-				if (predicate(i)) {
-					yield return i;
-				}
-			}
-		}
+		internal static IEnumerable<T> Filter<T>(this IEnumerable<T> self, Func<T, bool> f) =>
+			Enumerable.Where(self, f);
 
 		internal static U Reduce<T, U>(this IEnumerable<T> self, Func<U, T, U> f, U init) {
 			U acc = init;
