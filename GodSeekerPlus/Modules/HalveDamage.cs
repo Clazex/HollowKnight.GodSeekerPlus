38d45b4d43a50e6602cd624a1d80d6e2b587d06b
Frank Yang <clazex.y@gmail.com>
2022-01-06 23:17:22 +0800

    feat: rewrite halve damage to suppress multi-damage effect

--- GodSeekerPlus/Modules/HalveDamage.cs
+++ GodSeekerPlus/Modules/HalveDamage.cs
@@ -3,11 +3,11 @@ namespace GodSeekerPlus.Modules;
 [Category("BossChallenge")]
 internal sealed class HalveDamage : Module {
 	private protected override void Load() =>
-		ModHooks.TakeHealthHook += MakeDamageHalved;
+		ModHooks.AfterTakeDamageHook += MakeDamageHalved;
 
 	private protected override void Unload() =>
-		ModHooks.TakeHealthHook -= MakeDamageHalved;
+		ModHooks.AfterTakeDamageHook -= MakeDamageHalved;
 
-	private int MakeDamageHalved(int damage) =>
+	private int MakeDamageHalved(int _, int damage) =>
 		(int) Math.Ceiling(damage / 2d);
 }
