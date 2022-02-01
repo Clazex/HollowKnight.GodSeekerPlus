6fa002246aa5c3de04572eb05bed9101fc9b696d
Frank Yang <clazex.y@gmail.com>
2022-02-01 13:26:49 +0800

    feat: make satchel an optional dependency

--- GodSeekerPlus/GodSeekerPlus.cs
+++ GodSeekerPlus/GodSeekerPlus.cs
@@ -6,6 +6,17 @@ public sealed partial class GodSeekerPlus : Mod, ITogglableMod {
 
 	public override string GetVersion() => VersionUtil.Version.Value;
 
+	internal static bool satchelPresent = false;
+
+	static GodSeekerPlus() {
+		try {
+			DetectSatchel();
+		} catch (System.IO.FileNotFoundException) {
+		} catch (Exception e) {
+			Modding.Logger.LogError($"[GodSeekerPlus] - {e}");
+		}
+	}
+
 	public override void Initialize() {
 		if (Instance != null) {
 			Logger.LogWarn("Attempting to initialize multiple times, operation rejected");
@@ -21,5 +32,13 @@ public sealed partial class GodSeekerPlus : Mod, ITogglableMod {
 		ModuleManager.Unload();
 
 		Instance = null;
+	}
+
+	private static void DetectSatchel() {
+		try {
+			Activator.CreateInstance<Satchel.BetterMenus.Menu>();
+		} catch (MissingMethodException) {
+			satchelPresent = true;
+		}
 	}
 }
--- GodSeekerPlus/Menu.cs
+++ GodSeekerPlus/Menu.cs
@@ -1,3 +1,6 @@
+using Modding.Menu;
+using Modding.Menu.Config;
+
 using Satchel.BetterMenus;
 
 namespace GodSeekerPlus;
@@ -5,48 +8,93 @@ namespace GodSeekerPlus;
 public sealed partial class GodSeekerPlus : ICustomMenuMod {
 	bool ICustomMenuMod.ToggleButtonInsideMenu => true;
 
-	private static string[] States => new string[] {
-		Lang.Get("MOH_OFF", "MainMenu"),
-		Lang.Get("MOH_ON", "MainMenu")
-	};
+	public MenuScreen GetMenuScreen(MenuScreen modListMenu, ModToggleDelegates? toggleDelegates) =>
+		satchelPresent
+			? ModMenu.GetSatchelMenuScreen(modListMenu, toggleDelegates)
+			: ModMenu.GetFallbackMenuScreen(modListMenu, toggleDelegates);
 
-	public MenuScreen GetMenuScreen(MenuScreen modListMenu, ModToggleDelegates? toggleDelegates) {
-		Menu menu = new("ModName".Localize(), new[] {
-			toggleDelegates!.Value.CreateToggle(
-				"ModName".Localize(),
-				"ToggleButtonDesc".Localize(),
-				States[1],
-				States[0]
-			)
-		});
+	private static class ModMenu {
+		private static string[] StateStrings => new string[] {
+			Lang.Get("MOH_OFF", "MainMenu"),
+			Lang.Get("MOH_ON", "MainMenu")
+		};
 
-		ModuleManager
-			.Modules
-			.Values
-			.Filter(module => !module.Hidden)
-			.GroupBy(module => module.Category)
-			.OrderBy(group => group.Key)
-			.OrderBy(group => group.Key == nameof(Modules.Misc))
-			.Map(group => Blueprints.NavigateToMenu(
-				$"Categories/{group.Key}".Localize(),
-				"",
-				() => BuildSubMenu(menu.menuScreen, group.Key, group)
-			))
-			.ForEach(menu.AddElement);
+		internal static MenuScreen GetSatchelMenuScreen(MenuScreen modListMenu, ModToggleDelegates? toggleDelegates) {
+			Menu menu = new("ModName".Localize(), new[] {
+				toggleDelegates!.Value.CreateToggle(
+					"ModName".Localize(),
+					"ToggleButtonDesc".Localize(),
+					StateStrings[1],
+					StateStrings[0]
+				)
+			});
 
-		return menu.GetMenuScreen(modListMenu);
+			ModuleManager
+				.Modules
+				.Values
+				.Filter(module => !module.Hidden)
+				.GroupBy(module => module.Category)
+				.OrderBy(group => group.Key)
+				.OrderBy(group => group.Key == nameof(Modules.Misc))
+				.Map(group => Blueprints.NavigateToMenu(
+					$"Categories/{group.Key}".Localize(),
+					"",
+					() => new Menu(
+						$"Categories/{group.Key}".Localize(),
+						group.Map(module => new HorizontalOption(
+							$"Modules/{module.Name}".Localize(),
+							$"ToggleableLevel/{module.ToggleableLevel}".Localize(),
+							StateStrings,
+							(val) => module.Enabled = Convert.ToBoolean(val),
+							() => Convert.ToInt32(module.Enabled)
+						))
+						.ToArray()
+					).GetMenuScreen(menu.menuScreen)
+				))
+				.ForEach(menu.AddElement);
+
+			return menu.GetMenuScreen(modListMenu);
+		}
+
+		internal static MenuScreen GetFallbackMenuScreen(MenuScreen modListMenu, ModToggleDelegates? toggleDelegates) {
+			Logger.LogWarn("Satchel not found, using fallback menu");
+
+			MenuBuilder builder = MenuUtils.CreateMenuBuilderWithBackButton("ModName".Localize(), modListMenu, out _);
+			var toggler = (ModToggleDelegates) toggleDelegates!;
+
+			builder.AddContent(
+				RegularGridLayout.CreateVerticalLayout(105f),
+				c => {
+					c.AddHorizontalOption(
+						"ModName".Localize(),
+						new() {
+							ApplySetting = (_, i) => toggler.SetModEnabled(Convert.ToBoolean(i)),
+							RefreshSetting = (settings, _) =>
+								settings.optionList.SetOptionTo(Convert.ToInt32(toggler.GetModEnabled())),
+							CancelAction = _ => UIManager.instance.GoToDynamicMenu(modListMenu),
+							Description = null,
+							Label = "ModName".Localize(),
+							Options = StateStrings,
+							Style = HorizontalOptionStyle.VanillaStyle
+						},
+						out UnityEngine.UI.MenuOptionHorizontal toggle
+					);
+					toggle.menuSetting.RefreshValueFromGameSettings();
+
+					c.AddTextPanel(
+						"SatchelNotFoundPrompt",
+						new RelVector2(new Vector2(1500f, 105f)),
+						new() {
+							Text = "SatchelNotFoundPrompt".Localize(),
+							Anchor = TextAnchor.MiddleCenter,
+							Font = TextPanelConfig.TextFont.TrajanBold,
+							Size = 46,
+						}
+					);
+				}
+			);
+
+			return builder.Build();
+		}
 	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	private MenuScreen BuildSubMenu(MenuScreen parent, string name, IEnumerable<Module> modules) => new Menu(
-		$"Categories/{name}".Localize(),
-		modules.Map(module => new HorizontalOption(
-			$"Modules/{module.Name}".Localize(),
-			$"ToggleableLevel/{module.ToggleableLevel}".Localize(),
-			States,
-			(val) => module.Enabled = Convert.ToBoolean(val),
-			() => Convert.ToInt32(module.Enabled)
-		))
-		.ToArray()
-	).GetMenuScreen(parent);
 }
