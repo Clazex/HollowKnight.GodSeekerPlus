871c1aac8b37740cdfbd3976374cf63075a1ed5f
Frank Yang <clazex.y@gmail.com>
2022-02-21 19:48:15 +0800

    feat: more pantheon caps

---
+++ GodSeekerPlus/Modules/Visual/MorePantheonCaps.cs
@@ -0,0 +1,68 @@
+using Bindings = BossSequenceController.ChallengeBindings;
+
+namespace GodSeekerPlus.Modules.Visual;
+
+[Category(nameof(Visual))]
+[ToggleableLevel(ToggleableLevel.ChangeScene)]
+[DefaultEnabled]
+internal sealed class MorePantheonCaps : Module {
+	private static readonly Dictionary<string, int> doorPDDict = new() {
+		{ "bossDoorStateTier1", 1 },
+		{ "bossDoorStateTier2", 2 },
+		{ "bossDoorStateTier3", 3 },
+		{ "bossDoorStateTier4", 4 },
+		{ "bossDoorStateTier5", 5 }
+	};
+
+	private protected override void Load() {
+		On.BossSequenceDoor.Start += SetupCaps;
+		On.BossDoorChallengeCompleteUI.Start += RecordRAB;
+	}
+
+	private protected override void Unload() {
+		On.BossSequenceDoor.Start -= SetupCaps;
+		On.BossDoorChallengeCompleteUI.Start -= RecordRAB;
+	}
+
+	private void SetupCaps(On.BossSequenceDoor.orig_Start orig, BossSequenceDoor self) {
+		if (self.bossSequence != null) {
+			if (
+				self.completedNoHitsDisplay == null
+				&& self.gameObject.Child("Main Caps", "GG_door_cap_complete_nohits") is GameObject go
+			) {
+				self.completedNoHitsDisplay = go;
+				Logger.LogDebug($"Radiant cap enabled for {self.bossSequence.name}");
+			}
+
+			if (Ref.LS.GetRABCompletion(doorPDDict[self.playerDataString])
+				&& self.completedNoHitsDisplay?.GetComponent<SpriteRenderer>() is SpriteRenderer sr
+			) {
+				sr.transform.Translate(0, 0, -0.0801f);
+				sr.color = Color.black;
+
+				Logger.LogDebug($"Radiant AB effect enabled for {self.bossSequence.name}");
+			}
+		}
+
+		orig(self);
+	}
+
+	private void RecordRAB(On.BossDoorChallengeCompleteUI.orig_Start orig, BossDoorChallengeCompleteUI self) {
+		var currentData = (BossSequenceController.BossSequenceData) ReflectionHelper
+			.GetFieldInfo(typeof(BossSequenceController), "currentData", false)
+			.GetValue(null);
+
+		int num = doorPDDict[currentData.playerData];
+		bool rab = !currentData.knightDamaged
+			&& currentData.bindings == (Bindings.Nail | Bindings.Shell | Bindings.Charms | Bindings.Soul);
+		bool rabPrev = Ref.LS.GetRABCompletion(num);
+
+		if (rab && !rabPrev) {
+			Ref.LS.SetRABCompletion(num, true);
+			Logger.LogDebug($"Radiant AB in Pantheon #{num} recorded");
+		}
+
+		orig(self);
+	}
+
+}
--- GodSeekerPlus/Settings/LocalSettings.cs
+++ GodSeekerPlus/Settings/LocalSettings.cs
@@ -5,4 +5,35 @@ public sealed class LocalSettings {
 	public bool boundHeart = false;
 	public bool boundCharms = false;
 	public bool boundSoul = false;
+
+	[JsonIgnore]
+	private int rabCompletion = 0;
+
+	[JsonProperty(PropertyName = nameof(rabCompletion))]
+	public int RABCompletion {
+		get => rabCompletion;
+		set => rabCompletion = ((value & 0b11111) == value) ? value : 0;
+	}
+
+
+	#region RAB Completions Getter/Setter
+
+	public bool GetRABCompletion(int num) => num >= 1 && num <= 5
+		? (RABCompletion & (1 << (num - 1))) != 0
+		: throw new ArgumentOutOfRangeException(nameof(num));
+
+	internal void SetRABCompletion(int num, bool completed) {
+		if (num < 1 || num > 5) {
+			throw new ArgumentOutOfRangeException(nameof(num));
+		}
+
+		int mask = 1 << (num - 1);
+		if (completed) {
+			RABCompletion |= mask;
+		} else {
+			RABCompletion &= ~mask;
+		}
+	}
+
+	#endregion
 }
