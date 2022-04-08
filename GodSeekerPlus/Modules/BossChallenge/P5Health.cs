4e2292a1554ba1935c8840912bc002a59dea9148
Frank Yang <clazex.y@gmail.com>
2022-04-08 12:58:55 +0800

    fix: wrong damage

--- GodSeekerPlus/Modules/BossChallenge/P5Health.cs
+++ GodSeekerPlus/Modules/BossChallenge/P5Health.cs
@@ -1,16 +1,33 @@
+using MonoMod.RuntimeDetour;
+
 namespace GodSeekerPlus.Modules.BossChallenge;
 
 [ToggleableLevel(ToggleableLevel.ChangeScene)]
 internal sealed class P5Health : Module {
-	private protected override void Load() =>
-		On.BossSceneController.Awake += OverrideLevel;
+	private Detour? detour = null;
 
-	private protected override void Unload() =>
-		On.BossSceneController.Awake -= OverrideLevel;
+	private protected override void Load() {
+		detour = new(
+			ReflectionHelper.GetPropertyInfo(typeof(BossSceneController), "BossLevel").GetGetMethod(),
+			ReflectionHelper.GetMethodInfo(typeof(P5Health), nameof(OverrideLevel))
+		);
 
-	private void OverrideLevel(On.BossSceneController.orig_Awake orig, BossSceneController self) {
-		orig(self);
-
-		self.BossLevel = 0;
+		ModHooks.TakeDamageHook += FixDamage;
 	}
+
+	private protected override void Unload() {
+		detour?.Dispose();
+
+		ModHooks.TakeDamageHook -= FixDamage;
+	}
+
+	private int OverrideLevel() => 0;
+
+	private int FixDamage(ref int hazardType, int damage) => BossSceneController.IsBossScene
+		? ReflectionHelper.GetField<BossSceneController, int>(BossSceneController.Instance, "bossLevel") switch {
+			1 => damage * 2,
+			2 => 9999,
+			_ => damage,
+		}
+		: damage;
 }
