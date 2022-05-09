2e235d927079f78d0c0e46b185fde4011ef69bbe
HKLab <60681033+HKLab@users.noreply.github.com>
2022-05-09 13:03:58 +0800

    fix: infinite death in the pantheon

--- GodSeekerPlus/Modules/Bugfix/TransitionDeath.cs
+++ GodSeekerPlus/Modules/Bugfix/TransitionDeath.cs
@@ -2,40 +2,16 @@ namespace GodSeekerPlus.Modules.Bugfix;
 
 [DefaultEnabled]
 internal sealed class TransitionDeath : Module {
-	private bool deadInSequence = false;
-
 	private protected override void Load() {
 		On.HeroController.Die += RecordDeath;
-		On.GameManager.EnterHero += CheckDeath;
 	}
 
 	private protected override void Unload() {
-		deadInSequence = false;
 		On.HeroController.Die -= RecordDeath;
-		On.GameManager.EnterHero -= CheckDeath;
 	}
 
 	private IEnumerator RecordDeath(On.HeroController.orig_Die orig, HeroController self) {
-		if (BossSequenceController.IsInSequence) {
-			deadInSequence = true;
-		}
-
 		yield return orig(self);
-	}
-
-	private void CheckDeath(On.GameManager.orig_EnterHero orig, GameManager self, bool additiveGateSearch) {
-		orig(self, additiveGateSearch);
-
-		if (self.RespawningHero || GameManagerR.hazardRespawningHero) {
-			return;
-		}
-
-		if (deadInSequence && BossSequenceController.IsInSequence) {
-			Logger.LogWarn("Dead in sequence while not finishing it, trying again");
-			Ref.HC.StopCoroutine("Die");
-			Ref.HC.StartCoroutine("Die");
-		} else {
-			deadInSequence = false;
-		}
+		HeroControllerR.cState.dead = true;
 	}
 }
