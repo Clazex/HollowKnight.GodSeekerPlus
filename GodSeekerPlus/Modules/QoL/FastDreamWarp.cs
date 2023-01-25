214a7e434ba1b6f0076f5269961a383f1590244f
royitaqi <royitaqi@users.noreply.github.com>
2023-01-25 14:52:48 -0800

    Make hero invincible during dream nail/warp animation
    IMPORTANT: This affects dream nail and dream gate set, too.
    
    The original intention is to only cover dream warp's whole animation. Sadly, because dream nail/set/warp share the same starting animation, this starting animation need to be invincible. So in this implementation I'm making all dream nail/set/warp invincible.
    
    An improvement can be made to test UP being pressed right before/at start of "Start" state, so that invincibility is only set for dream warp (assuming that the player doesn't change UP/DOWN buttons during the start/charge animation). This involves changing more things in that FSM, but is feasible.

--- GodSeekerPlus/Modules/QoL/FastDreamWarp.cs
+++ GodSeekerPlus/Modules/QoL/FastDreamWarp.cs
@@ -18,10 +18,34 @@ internal sealed class FastDreamWarp : Module {
 		}
 	}
 
-	private static void ModifyDreamNailFSM(PlayMakerFSM fsm) =>
+	private static void ModifyDreamNailFSM(PlayMakerFSM fsm) {
 		fsm.InsertCustomAction("Warp Charge", (fsm) => {
 			if (BossSceneController.IsBossScene && ModuleManager.TryGetActiveModule<FastDreamWarp>(out _)) {
 				fsm.SendEvent("CHARGED");
 			}
 		}, 0);
+
+		bool? origInvincibility = null;
+		fsm.InsertCustomAction("Start", (fsm) => {
+			if (BossSceneController.IsBossScene && PlayerData.instance != null) {
+				origInvincibility = PlayerData.instance.isInvincible;
+				PlayerData.instance.isInvincible = true;
+			}
+		}, 0);
+		fsm.InsertCustomAction("Warp End", (fsm) => {
+			if (BossSceneController.IsBossScene && PlayerData.instance != null && origInvincibility != null) {
+				PlayerData.instance.isInvincible = origInvincibility.Value;
+			}
+		}, 0);
+		fsm.InsertCustomAction("Warp Cancel", (fsm) => {
+			if (BossSceneController.IsBossScene && PlayerData.instance != null && origInvincibility != null) {
+				PlayerData.instance.isInvincible = origInvincibility.Value;
+			}
+		}, 0);
+		fsm.InsertCustomAction("Inactive", (fsm) => {
+			if (BossSceneController.IsBossScene && PlayerData.instance != null && origInvincibility != null) {
+				PlayerData.instance.isInvincible = origInvincibility.Value;
+			}
+		}, 0);
+	}
 }
