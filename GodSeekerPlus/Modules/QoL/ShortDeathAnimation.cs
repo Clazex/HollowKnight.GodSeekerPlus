4a2094291a597ee21179923d0a3c40f712429b38
Frank Yang <clazex.y@gmail.com>
2023-01-27 04:06:16 +0800

    feat: make short death animation toggleable any time

--- GodSeekerPlus/Modules/QoL/ShortDeathAnimation.cs
+++ GodSeekerPlus/Modules/QoL/ShortDeathAnimation.cs
@@ -1,31 +1,22 @@
+using Osmi.FsmActions;
+
 namespace GodSeekerPlus.Modules.QoL;
 
 public sealed class ShortDeathAnimation : Module {
-	private static readonly GameObjectRef deathRef =
-		new(GameObjectRef.DONT_DESTROY_ON_LOAD, "Knight", "Hero Death");
-
 	public override bool DefaultEnabled => true;
 
-	public override ToggleableLevel ToggleableLevel => ToggleableLevel.ReloadSave;
+	public ShortDeathAnimation() =>
+		On.HeroController.Start += ModifyHeroDeathFSM;
 
-	private protected override void Load() =>
-		On.PlayMakerFSM.Start += ModifyHeroDeathFSM;
-
-	private protected override void Unload() =>
-		On.PlayMakerFSM.Start -= ModifyHeroDeathFSM;
-
-	private static void ModifyHeroDeathFSM(On.PlayMakerFSM.orig_Start orig, PlayMakerFSM self) {
+	private void ModifyHeroDeathFSM(On.HeroController.orig_Start orig, HeroController self) {
 		orig(self);
 
-		if (self.FsmName == "Hero Death Anim" && deathRef.MatchGameObject(self.gameObject)) {
-			ModifyHeroDeathFSM(self);
+		ModifyHeroDeathFSM(self.heroDeathPrefab.LocateMyFSM("Hero Death Anim"));
 
-			Logger.LogDebug("Hero Death FSM modified");
-		}
+		Logger.LogDebug("Hero Death FSM modified");
 	}
 
-	private static void ModifyHeroDeathFSM(PlayMakerFSM fsm) =>
-		fsm.InsertAction("Bursting", new GGCheckIfBossScene() {
-			bossSceneEvent = FsmEvent.Finished
-		}, 5);
+	private void ModifyHeroDeathFSM(PlayMakerFSM fsm) => fsm.AddAction("Bursting",
+		new InvokePredicate(() => Loaded && BossSceneController.IsBossScene) { trueEvent = FsmEvent.Finished }
+	);
 }
